243e67d627f7bf248213a228f29704c0bcc7b506
ignaciojavier27 <nacholin124@gmail.com>
2024-08-25 10:51:18 -0400

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { ENDPOINT_FACT, ENDPOINT_IMG } from './constants';
 import { fetchFact } from './helpers/fetchFact.js';
+import './App.css'
 
 const App = () => {
     const [fact, setFact] = useState();
     const [imgUrl, setImgUrl] = useState();
     const [firstWord, setFirstWord] = useState();
     const [isLoading, setIsLoading] = useState(true);
+    const [getFact, setGetFact] = useState(true);
 
-    useEffect(() => {
+
+    const getRandomFact = () => {
+        setIsLoading(true)
         fetchFact({ endpointFact: ENDPOINT_FACT, endpointImg: ENDPOINT_IMG })
             .then(({ fact, newFirstWord, newImgUrl }) => {
                 setFact(fact);
@@ -18,44 +22,44 @@ const App = () => {
             })
             .catch(error => {
                 console.error('Error fetching fact:', error);
-                setIsLoading(false); // En caso de error, para no quedar en estado de carga
+                setIsLoading(false);
             });
-    }, []);
+    }
+
+    useEffect(getRandomFact, [getFact]);
 
     const handleImageLoad = () => {
         console.log('Cargó la imagen')
         setIsLoading(false)
     };
 
-    const Content = () => {
-        return (
-            <>
-                {}
-
-            </>
-        );
+    const handleClick = () => {
+        setGetFact(!getFact)
     }
 
     return (
         <main>
-            <h1>Fact Gatos</h1>
-            {
-                !isLoading
-                    ? fact && <p>{fact}</p>
-                    : 'Cargando...'
-            }
+            <h1>Fact Cats</h1>
+            <button onClick={handleClick}>New Fact</button>
+            <section className="container-info">
+                {
+                    imgUrl && (
+                        <img 
+                            src={imgUrl} 
+                            alt={firstWord} 
+                            width={500}
+                            onLoad={handleImageLoad}
+                            style={{ display: !isLoading ? 'block' : 'none' }}
+                        />
+                    )
+                }
+                {
+                    !isLoading
+                        ? fact && <p>{fact}</p>
+                        : 'Cargando...'
+                }
 
-            {
-                imgUrl && (
-                    <img 
-                        src={imgUrl} 
-                        alt={firstWord} 
-                        width={500}
-                        onLoad={handleImageLoad}
-                        style={{ display: !isLoading ? 'block' : 'none' }}
-                    />
-                )
-            }
+            </section>
         </main>
     );
 }
